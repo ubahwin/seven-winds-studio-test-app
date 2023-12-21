d6a3237e8ee7e3beac7a0d0a61815d6a023fe6cc
Ivan Vdovin <ivan03_vdovin@mail.ru>
2023-12-21 09:39:57 +0300

    Initial Commit

---
+++ SevenWindsStudio/SevenWindsStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenWindsStudioApp.swift
+//  SevenWindsStudio
+//
+//  Created by Иван Вдовин on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SevenWindsStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
